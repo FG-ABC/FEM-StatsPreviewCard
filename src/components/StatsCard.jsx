f1014d03d0c5751faa1ad9e8476e5dbd9f633a6e
ANGlenn <fghernandez108@gmail.com>
2024-04-04 10:05:40 +0800

    Upload screenshot
    Upload screenshot and fixed top right border radius for desktop view

--- src/components/StatsCard.jsx
+++ src/components/StatsCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const StatsCard = () => {
   return (
     <div id='Card' className='w-8/12 sm:w-auto sm:h-96 flex flex-col rounded-xl sm:flex-row'>
-        <div id='Imageholder' className='rounded-t-xl sm:rounded-t-none sm:rounded-r-xl w-full h-52 sm:h-full sm:w-1/2 sm:order-2'>      
+        <div id='Imageholder' className='rounded-t-xl sm:rounded-l-none sm:rounded-r-xl w-full h-52 sm:h-full sm:w-1/2 sm:order-2'>      
         </div>
         <div id='Contentholder' className='p-8 flex flex-col items-center sm:justify-between sm:p-12 sm:pr-14 sm:w-1/2'>
             <h1 className='text-3xl text-center sm:text-left'>Get <span id='Violet'>insights</span> that
